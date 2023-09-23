f6da9189cd67191c9ff4864a7068254d3a3532d3
alivasisari <alivasisari1380@gmail.com>
2023-09-23 21:30:05 +0330

    Add models to admin-panel and migrations

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.Author)
+admin.site.register(models.Ebook)
+admin.site.register(models.Category)
