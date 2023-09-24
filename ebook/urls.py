169a9affe8c25a3c7c600f7d232c77b2f7388396
alivasisari <alivasisari1380@gmail.com>
2023-09-24 20:47:14 +0330

    Add ebooks app url

--- ebook/urls.py
+++ ebook/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("api/v1/rest-auth/", include("dj_rest_auth.urls")),
     path("api/v1/rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     path('api-token-auth/', views.obtain_auth_token, name="api-token-auth")
+    path('api/v1/ebboks/', views.obtain_auth_token)
 ]
