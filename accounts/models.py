1885a3a735cb3a219de73afd805f2bbf11c3b75e
alivasisari <alivasisari1380@gmail.com>
2023-09-24 17:39:19 +0330

    Add user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        UNSET = 'MF', 'Unset'
+        
+    gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.UNSET)
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        if self.first_name is not '' and self.last_name is not '':
+            return self.get_full_name()
+
+        return self.email
