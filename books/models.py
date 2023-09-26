75231ee733acefa0ea35d8a0cd377673444b5dba
alivasisari <alivasisari1380@gmail.com>
2023-09-26 22:06:19 +0330

    Fix models slug blank and Set category and author image blank =true

--- books/models.py
+++ books/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class Author(models.Model):
     """Model representing an author."""
     full_name = models.CharField(max_length=100, verbose_name='FullName')
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True, max_length=200, unique=True, verbose_name='Slug')
+    slug = models.SlugField(default="", null=False, db_index=True, blank=False, max_length=200, unique=True, verbose_name='Slug')
     bio = models.TextField(verbose_name='Bio')
-    image = models.FileField(upload_to = "uploads/authors/images/", verbose_name='image')
+    image = models.FileField(upload_to = "authors/images/", null=True, blank=True, verbose_name='image')
     date_of_birth = models.DateField(null=True, blank=True, verbose_name='DateOfBirth')
     date_of_death = models.DateField(null=True, blank=True, verbose_name='DateOfDeath')
     created = models.DateTimeField(auto_now_add=True)
@@ -26,8 +26,8 @@ class Author(models.Model):
 # Category model
 class Category(models.Model):
     name = models.CharField(max_length=200, null=True, verbose_name='Name')
-    icon = models.FileField(upload_to = "uploads/category/icons/", verbose_name='Icon')
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True, max_length=200, unique=True, verbose_name='Slug')
+    icon = models.FileField(upload_to = "category/icons/", null=True, blank=True, verbose_name='Icon')
+    slug = models.SlugField(default="", null=False, db_index=True, blank=False, max_length=200, unique=True, verbose_name='Slug')
     description = models.CharField(max_length=500, null=True,blank=True, verbose_name='Description')
     is_active = models.BooleanField(default=False, verbose_name='IsActive')
     created = models.DateTimeField(auto_now_add=True)
@@ -44,13 +44,13 @@ class Category(models.Model):
 class Ebook(models.Model):
     title = models.CharField(max_length=200, verbose_name='Title')
     authors = models.ManyToManyField(Author, related_name='books', verbose_name='Authors')
-    pages = models.IntegerField(verbose_name='NumberOfPages')
+    pages = models.IntegerField(null=True, blank=True, verbose_name='NumberOfPages')
     price = models.FloatField(verbose_name='Price')
-    cover = models.ImageField(upload_to='uploads/ebooks/covers/', verbose_name='Cover')
-    slug = models.SlugField(default="", null=False, db_index=True, blank=True, max_length=200, unique=True, verbose_name='Slug')
+    cover = models.ImageField(upload_to='ebooks/covers/', verbose_name='Cover')
+    slug = models.SlugField(default="", null=False, db_index=True, blank=False, max_length=200, unique=True, verbose_name='Slug')
     description = models.TextField(verbose_name='Description')
     # this will be the download link of the ebook
-    ebook = models.FileField(upload_to='uploads/ebooks/files/', verbose_name='EBook')
+    ebook = models.FileField(upload_to='ebooks/files/', verbose_name='EBook')
     is_active = models.BooleanField(default=False, verbose_name='IsActive')
     # has one to many relationship with Category
     category = models.ManyToManyField(Category, related_name='books', verbose_name='Categories')
