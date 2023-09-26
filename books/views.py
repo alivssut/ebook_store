b27d131bb05f26d2479cad367a4a5158c199bf05
alivasisari <alivasisari1380@gmail.com>
2023-09-26 21:10:00 +0330

    Add session and token authentication to view

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from .models import Ebook
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import EbookSerializer
 from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 
 # Create your views here.
 
@@ -10,4 +11,5 @@ from rest_framework import generics
 class EbookListView(generics.ListAPIView):
     queryset = Ebook.objects.all()
     serializer_class = EbookSerializer
-    permission_classes = [AllowAny,]
+    permission_classes = [IsAuthenticated,]
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
