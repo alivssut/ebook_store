56e5033fa636c44a94686a8743b5260d08fcf312
alivasisari <alivasisari1380@gmail.com>
2023-09-26 21:32:27 +0330

    Add ebook detail view and url

--- books/urls.py
+++ books/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import EbookListView
+from .views import EbookListView, EbookDetailView
 
 urlpatterns = [
     path("ebooks/", EbookListView.as_view(), name="ebooks_list"),
+    path("ebooks/<slug:slug>/", EbookDetailView.as_view(), name="ebook_detail"),
 ]
--- books/views.py
+++ books/views.py
@@ -13,3 +13,13 @@ class EbookListView(generics.ListAPIView):
     serializer_class = EbookSerializer
     permission_classes = [IsAuthenticated,]
     authentication_classes = [SessionAuthentication, TokenAuthentication]
+    
+
+# ebook detail view
+class EbookDetailView(generics.RetrieveAPIView):
+    serializer_class = EbookSerializer
+    authentication_classes = [SessionAuthentication, TokenAuthentication]
+    permission_classes = [IsAuthenticated,]
+    lookup_url_kwarg  = 'slug'
+    lookup_field = 'slug'
+    queryset = Ebook.objects.all()
